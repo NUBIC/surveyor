ddb76f00e6b76452d514c072f29f1918f4dca6c9
Mark Yoon <yoon@northwestern.edu>
2009-09-23 12:48:31 -0500

    namespacing routes. closes #2

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,10 @@
 ActionController::Routing::Routes.draw do |map|
-  map.with_options :controller => 'surveying' do |surveying|
-      surveying.available_surveys '/',                                      :conditions => {:method => :get}, :action => "new"      # GET survey list
-      surveying.take_survey       ':survey_code',                           :conditions => {:method => :post}, :action => "create"  # Only POST of survey to cre
-      surveying.view_my_survey    ':survey_code/:response_set_code',        :conditions => {:method => :get}, :action => "show"     # GET viewable/printable? su
-      surveying.edit_my_survey    ':survey_code/:response_set_code/take',   :conditions => {:method => :get}, :action => "edit"     # GET editable survey 
-      surveying.update_my_survey  ':survey_code/:response_set_code',        :conditions => {:method => :put}, :action => "update"   # PUT edited survey 
-      surveying.finish_my_survey  ':survey_code/:response_set_code/finish', :conditions => {:method => :put}, :action => "finish"   # PUT to close out the respo
+  map.with_options :controller => 'surveying' do |s|
+    s.available_surveys 'surveys/',                                       :conditions => {:method => :get}, :action => "new"      # GET survey list
+    s.take_survey       'surveys/:survey_code',                           :conditions => {:method => :post}, :action => "create"  # Only POST of survey to cre
+    s.view_my_survey    'surveys/:survey_code/:response_set_code',        :conditions => {:method => :get}, :action => "show"     # GET viewable/printable? su
+    s.edit_my_survey    'surveys/:survey_code/:response_set_code/take',   :conditions => {:method => :get}, :action => "edit"     # GET editable survey 
+    s.update_my_survey  'surveys/:survey_code/:response_set_code',        :conditions => {:method => :put}, :action => "update"   # PUT edited survey 
+    s.finish_my_survey  'surveys/:survey_code/:response_set_code/finish', :conditions => {:method => :put}, :action => "finish"   # PUT to close out the respo
   end 
-
-  map.resources :surveys
-  map.resources :sections
-  map.resources :questions
-  map.resources :answers
-  map.resources :dependencies
-  map.resources :dependency_conditions
-  map.resources :response_sets
 end
-
-
