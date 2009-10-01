e2632e482a695f546fe4d57f0c10f2396492ee14
Mark Yoon <yoon@northwestern.edu>
2009-10-01 11:32:47 -0500

    adding relative url root for surveyor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 ActionController::Routing::Routes.draw do |map|
+  root = Surveyor::Config['default.relative_url_root'] || "surveys/"
   map.with_options :controller => 'surveying' do |s|
-    s.available_surveys 'surveys/',                                       :conditions => {:method => :get}, :action => "new"      # GET survey list
-    s.take_survey       'surveys/:survey_code',                           :conditions => {:method => :post}, :action => "create"  # Only POST of survey to cre
-    s.view_my_survey    'surveys/:survey_code/:response_set_code',        :conditions => {:method => :get}, :action => "show"     # GET viewable/printable? su
-    s.edit_my_survey    'surveys/:survey_code/:response_set_code/take',   :conditions => {:method => :get}, :action => "edit"     # GET editable survey 
-    s.update_my_survey  'surveys/:survey_code/:response_set_code',        :conditions => {:method => :put}, :action => "update"   # PUT edited survey 
+    s.available_surveys "#{root}",                                       :conditions => {:method => :get}, :action => "new"      # GET survey list
+    s.take_survey       "#{root}:survey_code",                           :conditions => {:method => :post}, :action => "create"  # Only POST of survey to cre
+    s.view_my_survey    "#{root}:survey_code/:response_set_code",        :conditions => {:method => :get}, :action => "show"     # GET viewable/printable? su
+    s.edit_my_survey    "#{root}:survey_code/:response_set_code/take",   :conditions => {:method => :get}, :action => "edit"     # GET editable survey 
+    s.update_my_survey  "#{root}:survey_code/:response_set_code",        :conditions => {:method => :put}, :action => "update"   # PUT edited survey 
   end 
 end
--- generators/surveyor/templates/initializers/surveyor.rb
+++ generators/surveyor/templates/initializers/surveyor.rb
@@ -1,4 +1,5 @@
 Surveyor::Config.run do |config|
+  config['default.relative_url_root'] = nil # "surveys/"
   config['default.title'] = nil # "You can take these surveys:"
   config['default.layout'] = nil # "surveyor_default"
   config['default.finish'] =  nil # "/surveys" # or :finish_path_method # Proc.new{ generate_path }
