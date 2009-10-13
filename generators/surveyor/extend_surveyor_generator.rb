cfb35222cc45960d75ad12ce93614bf2b2db17e8
Mark Yoon <yoon@northwestern.edu>
2009-10-13 11:43:27 -0500

    extending surveyor generator

--- generators/surveyor/extend_surveyor_generator.rb
+++ generators/surveyor/extend_surveyor_generator.rb
@@ -12,7 +12,7 @@ class ExtendSurveyorGenerator < Rails::Generator::Base
       # http://www.redmine.org/boards/3/topics/4095#message-4136
       # http://blog.mattwynne.net/2009/07/11/rails-tip-use-polymorphism-to-extend-your-controllers-at-runtime/
       m.file "extensions/survey_extensions.rb", "app/models/survey_extensions.rb" 
-      m.file "extensions/surveyor_helper_extensions.rb", "app/helper/surveyor_helper_extensions.rb"
+      m.file "extensions/surveyor_helper_extensions.rb", "app/helpers/surveyor_helper_extensions.rb"
       m.file "extensions/surveyor_controller_extensions.rb", "app/controllers/surveyor_controller_extensions.rb"
       
       m.readme "EXTENDING_SURVEYOR"
