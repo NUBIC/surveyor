8721345351089f3b7dbc832d0e7b6907e7890052
Mark Yoon <yoon@northwestern.edu>
2009-11-03 13:25:04 -0600

    add base to spec helper

--- script/surveyor/specs/spec_helper.rb
+++ script/surveyor/specs/spec_helper.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..'))
 require File.expand_path(File.dirname(__FILE__) + "/../../../lib/surveyor")
+require File.expand_path(File.dirname(__FILE__) + "/../base")
 
 Spec::Runner.configure do |config|
 end
