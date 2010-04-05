8d6dab556cde44a41c10a50cb400921373baab72
Mark Yoon <yoon@northwestern.edu>
2010-04-05 13:47:17 -0500

    automatically add backslashes and eliminate multiple backslashes in relative root for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
-  root = Surveyor::Config['default.relative_url_root'] || "surveys/"
+  root = Surveyor::Config['default.relative_url_root'] || "surveys"
+  root = (root << "/").gsub(/\/+/, "/")
   map.with_options :controller => 'surveyor' do |s|
     s.available_surveys "#{root}",                                       :conditions => {:method => :get}, :action => "new"      # GET survey list
     s.take_survey       "#{root}:survey_code",                           :conditions => {:method => :post}, :action => "create"  # Only POST of survey to create
