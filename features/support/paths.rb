b6bcc8d8ca742f3dee1c6b0428dcace2e7b72838
Mark Yoon <yoon@northwestern.edu>
2010-04-05 10:57:52 -0500

    upgrading cucumber to 0.6

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -3,16 +3,16 @@ module NavigationHelpers
   #
   #   When /^I go to (.+)$/ do |page_name|
   #
-  # step definition in webrat_steps.rb
+  # step definition in web_steps.rb
   #
   def path_to(page_name)
     case page_name
     
-    when /the homepage/
+    when /the home\s?page/
       '/'
     
     # Add more mappings here.
-    # Here is a more fancy example:
+    # Here is an example that pulls values out of the Regexp:
     #
     #   when /^(.*)'s profile page$/i
     #     user_profile_path(User.find_by_login($1))
@@ -23,5 +23,3 @@ module NavigationHelpers
     end
   end
 end
-
-World(NavigationHelpers)
