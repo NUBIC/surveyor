716b31689c572e81040d0eae735daaea4a55b962
Mark Yoon <yoon@northwestern.edu>
2010-06-18 15:59:13 -0500

    fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   match 'surveys/:survey_code', :to                         => 'surveyor#create', :as => 'take_survey', :via       => :post
   match 'surveys/:survey_code/:response_set_code', :to      => 'surveyor#show', :as   => 'view_my_survey', :via    => :get
   match 'surveys/:survey_code/:response_set_code/take', :to => 'surveyor#edit', :as   => 'edit_my_survey', :via    => :get
-  match 'surveys/:survey_code/:response_set_code', :to      => 'surveyor#update', :as => 'update_my_survey', :via  => :get
+  match 'surveys/:survey_code/:response_set_code', :to      => 'surveyor#update', :as => 'update_my_survey', :via  => :put
 end
