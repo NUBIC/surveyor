9b1c33d420a268db3c6e43394d7026156ef90baf
Mark Yoon <yoon@northwestern.edu>
2010-06-18 15:23:07 -0500

    new rails 3 routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
-ActionController::Routing::Routes.draw do |map|
-  # root = Surveyor::Config['default.relative_url_root'] || "surveys"
-  root = "surveys"
-  root = (root << "/").gsub(/\/+/, "/")
-  map.with_options :controller => 'surveyor' do |s|
-    s.available_surveys "#{root}",                                        :conditions => {:method => :get}, :action => "new"                      # GET survey list
-    s.take_survey       "#{root}:survey_code",                            :conditions => {:method => :post}, :action => "create"                  # Only POST of survey to create
-    s.view_my_survey    "#{root}:survey_code/:response_set_code.:format", :conditions => {:method => :get}, :action => "show", :format => "html"  # GET viewable/printable? survey
-    s.edit_my_survey    "#{root}:survey_code/:response_set_code/take",    :conditions => {:method => :get}, :action => "edit"                     # GET editable survey 
-    s.update_my_survey  "#{root}:survey_code/:response_set_code",         :conditions => {:method => :put}, :action => "update"                   # PUT edited survey 
-  end   
+Rails.application.routes.draw do
+  match 'surveys', :to                                      => 'surveyor#new', :as    => 'available_surveys', :via => :get
+  match 'surveys/:survey_code', :to                         => 'surveyor#create', :as => 'take_survey', :via       => :post
+  match 'surveys/:survey_code/:response_set_code', :to      => 'surveyor#show', :as   => 'view_my_survey', :via    => :get
+  match 'surveys/:survey_code/:response_set_code/take', :to => 'surveyor#edit', :as   => 'edit_my_survey', :via    => :get
+  match 'surveys/:survey_code/:response_set_code', :to      => 'surveyor#update', :as => 'update_my_survey', :via  => :get
 end
