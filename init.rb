174c1f3cddeea4d925268f29490cc2bd58abbf56
Mark Yoon <yoon@northwestern.edu>
2010-06-18 15:32:59 -0500

    moving acts_as_response to init

--- init.rb
+++ init.rb
@@ -1,1 +1,2 @@
 # Init hook code here
+require 'surveyor/acts_as_response'
--- lib/surveyor.rb
+++ lib/surveyor.rb
@@ -1,5 +1,3 @@
-require 'surveyor/acts_as_response'
-
 module Surveyor
   require 'surveyor/engine' if defined?(Rails) && Rails::VERSION::MAJOR == 3
 end
