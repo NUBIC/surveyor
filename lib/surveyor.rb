957e701cd638e9f545ec6487e128c5613274812c
Mark Yoon <yoon@northwestern.edu>
2010-06-18 14:11:37 -0500

    moving tasks to engine

--- lib/surveyor.rb
+++ lib/surveyor.rb
@@ -1,27 +1,5 @@
-require 'generators/install_generator.rb'
-require 'generators/custom_generator.rb'
 require 'surveyor/acts_as_response'
 
 module Surveyor
   require 'surveyor/engine' if defined?(Rails) && Rails::VERSION::MAJOR == 3
-  # Tasks
-  class Railtie < Rails::Railtie
-    rake_tasks do
-      load "lib/tasks/surveyor_tasks.rake"
-    end
-  end
 end
-
-# From http://guides.rubyonrails.org/plugins.html#controllers
-# Fix for:
-# ArgumentError in SurveyorController#edit 
-# A copy of ApplicationController has been removed from the module tree but is still active!
-# Equivalent of using "unloadable" in SurveyorController (unloadable has been deprecated)
-
-# %w{models controllers}.each do |dir|
-#   path = File.expand_path(File.join(File.dirname(__FILE__), '../app', dir))
-#   # $LOAD_PATH << path # already here
-#   # ActiveSupport::Dependencies.load_paths << path # already here too
-#   ActiveSupport::Dependencies.load_once_paths.delete(path)
-#   # [$LOAD_PATH, ActiveSupport::Dependencies.load_paths, ActiveSupport::Dependencies.load_once_paths].each{|x| Rails.logger.info x}
-# end
--- lib/surveyor/engine.rb
+++ lib/surveyor/engine.rb
@@ -1,7 +1,11 @@
 require 'rails'
+require 'surveyor'
 
 module Surveyor
- class Engine < Rails::Engine
-    # engine_name :surveyor
+  class Engine < Rails::Engine
+    engine_name :surveyor
+    rake_tasks do
+      load "lib/tasks/surveyor_tasks.rake"
+    end
   end
 end
