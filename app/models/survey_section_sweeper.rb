74aab78a65ffe05e5fd1a098fec2a05b10c13ac5
Mark Yoon <yoon@northwestern.edu>
2010-08-31 15:40:04 -0500

    explicity require surveyor models and helper. update sweeper syntx. closes #77

--- app/models/survey_section_sweeper.rb
+++ app/models/survey_section_sweeper.rb
@@ -1,14 +1,14 @@
 class SurveySectionSweeper < ActionController::Caching::Sweeper
-  observer SurveySection
-
+  observe :survey_section
+  
   def after_save(section)
     expire_cache(section)
   end
-
+  
   def after_destroy(section)
     expire_cache(section)
   end
-
+  
   def expire_cache(section)
     expire_fregment "section_#{section.id}"
   end
--- lib/surveyor.rb
+++ lib/surveyor.rb
@@ -1,5 +1,6 @@
 require 'surveyor/common'
 require 'surveyor/acts_as_response'
 
-Dir.glob('app/models/*.rb').each{|f| require f}
-Dir.glob('app/helpers/*.rb').each{|f| require f}
+
+Dir.glob(File.join(File.dirname(__FILE__),'..','app','models','*.rb')).each{|f| require f}
+Dir.glob(File.join(File.dirname(__FILE__),'..','app','helpers','*.rb')).each{|f| require f}
