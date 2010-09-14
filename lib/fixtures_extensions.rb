53dc947f93f0c76f3dfeff56928a751ed606b1b7
Mark Yoon <yoon@northwestern.edu>
2010-09-14 09:09:04 -0500

    cleanup of old parsing strategy

--- lib/fixtures_extensions.rb
+++
@@ -1,6 +0,0 @@
-class Fixtures
-  def delete_existing_fixtures
-    # @connection.delete "DELETE FROM #{@connection.quote_table_name(table_name)}", 'Fixture Delete'
-    puts "Appending, skipping delete..."
-  end
-end
--- script/surveyor/specs/spec_helper.rb
+++
@@ -1,7 +0,0 @@
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..'))
-require File.expand_path(File.dirname(__FILE__) + "/../../../lib/surveyor")
-require File.expand_path(File.dirname(__FILE__) + "/../parser")
-require File.expand_path(File.dirname(__FILE__) + "/../base")
-
-Spec::Runner.configure do |config|
-end
