c3619a30cfdb9a448416cb23e3ceb170164ef556
Mark Yoon <yoon@northwestern.edu>
2010-09-23 09:21:37 -0500

    adjusting results routes. closes #80.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  match 'surveys', :to                                      => 'surveyor#new', :as    => 'available_surveys', :via          => :get
-  match 'surveys/:survey_code', :to                         => 'surveyor#create', :as => 'take_survey', :via                => :post
-  match 'surveys/:survey_code/:response_set_code', :to      => 'surveyor#show', :as   => 'view_my_survey', :via             => :get
-  match 'surveys/:survey_code/:response_set_code/take', :to => 'surveyor#edit', :as   => 'edit_my_survey', :via             => :get
-  match 'surveys/:survey_code/:response_set_code', :to      => 'surveyor#update', :as => 'update_my_survey', :via           => :put
-  match 'results/results', :to                              => 'results#index', :as   => 'show_sureveys_result_lists', :via => :get
-  match 'results/:survey_code', :to                         => 'results#show', :as    => 'show_one_survey_results', :via    => :get
-end
+  match 'surveys', :to                                      => 'surveyor#new', :as    => 'available_surveys', :via => :get
+  match 'surveys/:survey_code', :to                         => 'surveyor#create', :as => 'take_survey', :via       => :post
+  match 'surveys/:survey_code/:response_set_code', :to      => 'surveyor#show', :as   => 'view_my_survey', :via    => :get
+  match 'surveys/:survey_code/:response_set_code/take', :to => 'surveyor#edit', :as   => 'edit_my_survey', :via    => :get
+  match 'surveys/:survey_code/:response_set_code', :to      => 'surveyor#update', :as => 'update_my_survey', :via  => :put
+  match 'results', :to                                      => 'results#index', :as   => 'results', :via           => :get
+  match 'results/:survey_code', :to                         => 'results#show', :as    => 'result', :via            => :get
+end                                                                                      
