9dd5f2c454b6263d9cb32890d619c2d1d1650090
Brian Chamberlain <b-chamberlain@northwestern.edu>
2010-11-09 11:29:39 -0600

    un-dryed the gemfile to try to fix a gem location problem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,8 +7,11 @@ require "spec"
 
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 
-require File.join(File.dirname(__FILE__), 'test_app/spec/spec_helper')
-require File.dirname(__FILE__) + "/factories"
+
+this_dir = File.dirname(__FILE__)
+raise "Alert! Run the rake task to install the test Rails app. It seems to be missing." unless File.directory?(File.join(this_dir,'test_app/spec'))
+require File.join(this_dir, 'test_app/spec/spec_helper')
+require File.join(this_dir, '/factories')
 
 require 'surveyor'
 require 'surveyor/parser'
