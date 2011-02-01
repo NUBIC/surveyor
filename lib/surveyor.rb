d243b2c57f61b6f4e801f58608b8177360f5f66f
Mark Yoon <yoon@northwestern.edu>
2011-02-01 13:56:34 -0600

    rails 3 mods for surveyor setup

--- lib/surveyor.rb
+++ lib/surveyor.rb
@@ -1,6 +1,11 @@
+module Surveyor
+  require 'surveyor/engine' if defined?(Rails) && Rails::VERSION::MAJOR == 3
+end
 require 'surveyor/common'
 require 'surveyor/acts_as_response'
 require 'formtastic/surveyor_builder'
+# require 'surveyor/surveyor_controller_methods'
+# require 'surveyor/models/survey_methods'
 Formtastic::SemanticFormHelper.builder = Formtastic::SurveyorBuilder
 Formtastic::SemanticFormBuilder.default_text_area_height = 5
 Formtastic::SemanticFormBuilder.default_text_area_width = 50
--- lib/surveyor/engine.rb
+++ lib/surveyor/engine.rb
@@ -3,6 +3,7 @@ require 'surveyor'
 
 module Surveyor
   class Engine < Rails::Engine
+    config.autoload_paths << File.expand_path("../../", __FILE__)
     rake_tasks do
       load "tasks/surveyor_tasks.rake"
     end
