4cbe42534d6e5d1edcfc3cb870cb4305731895ae
Nataliya Shurupova <n-shurupova@northwestern.edu>
2011-06-01 10:48:40 -0500

    Adding a comment to describe the active-record workaround.

--- lib/surveyor/models/response_set_methods.rb
+++ lib/surveyor/models/response_set_methods.rb
@@ -140,9 +140,10 @@ module Surveyor
       end
 
       protected
-
+      
       def dependencies(question_ids = nil)
         deps = Dependency.all(:include => :dependency_conditions, :conditions => {:dependency_conditions => {:question_id => question_ids || responses.map(&:question_id)}})
+        # this is a work around for a bug in active_record in rails 2.3 which incorrectly eager-loads associatins when a condition clause includes an association limiter
         deps.each{|d| d.dependency_conditions.reload} 
         deps
       end
