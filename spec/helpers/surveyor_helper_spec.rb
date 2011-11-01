c96b57e4d0c1501c9274b27f758409191055c1a7
Paul Friedman <p-friedman@northwestern.edu>
2011-11-01 16:01:51 -0500

    set directory for asset based on configuration

--- spec/helpers/surveyor_helper_spec.rb
+++ spec/helpers/surveyor_helper_spec.rb
@@ -13,7 +13,10 @@ describe SurveyorHelper do
     helper.q_text(q1).should == "1) #{q1.text}"
     helper.q_text(q2).should == q2.text
     helper.q_text(q3).should == q3.text
-    helper.q_text(q4).should == '<img alt="Something" src="/images/something.jpg" />'
+    
+    dir = Rails.application.config.assets.enabled ? "assets" : "images"
+    
+    helper.q_text(q4).should == %Q(<img alt="Something" src="/#{dir}/something.jpg" />)
     helper.q_text(q5).should == q5.text
   end
   it "should return the group text with number" do
