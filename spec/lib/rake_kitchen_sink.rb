2888d267855a0817ace41c856d08935626f6b779
Mark Yoon <yoon@northwestern.edu>
2012-03-01 10:27:19 -0600

    test rake and the kitchen sink survey. closes #152

---
+++ spec/lib/rake_kitchen_sink.rb
@@ -0,0 +1,25 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require 'rake'
+
+describe Surveyor::Parser do
+  before do
+    @rake = Rake::Application.new
+    Rake.application = @rake
+    Rake.application.rake_require "lib/tasks/surveyor_tasks"
+    Rake::Task.define_task(:environment)
+  end
+  it "should return properly parse the kitchen sink survey" do
+    ENV["FILE"]="surveys/kitchen_sink_survey.rb"
+    @rake["surveyor"].invoke
+    
+    Survey.count.should == 1
+    SurveySection.count.should == 2 
+    Question.count.should == 44 
+    Answer.count.should == 241 
+    Dependency.count.should == 6 
+    DependencyCondition.count.should == 9 
+    QuestionGroup.count.should == 6
+
+    Survey.all.map(&:destroy)
+  end
+end
