cf30b17c0bc3d357e23d147b25a7ad178e81fb12
Mark Yoon <yoon@northwestern.edu>
2012-03-01 10:02:59 -0600

    handle utf-8 in surveys. closes #164

--- lib/surveyor/common.rb
+++ lib/surveyor/common.rb
@@ -14,11 +14,11 @@ module Surveyor
 
       def to_normalized_string(text)
         words_to_omit = %w(a be but has have in is it of on or the to when)
-        col_text = text.to_s.gsub(/(<[^>]*>)|\n|\t/s, ' ') # Remove html tags
+        col_text = text.to_s.gsub(/(<[^>]*>)|\n|\t/su, ' ') # Remove html tags
         col_text.downcase!                            # Remove capitalization
-        col_text.gsub!(/\"|\'/, '')                   # Remove potential problem characters
-        col_text.gsub!(/\(.*?\)/,'')                  # Remove text inside parens
-        col_text.gsub!(/\W/, ' ')                     # Remove all other non-word characters      
+        col_text.gsub!(/\"|\'/u, '')                   # Remove potential problem characters
+        col_text.gsub!(/\(.*?\)/u,'')                  # Remove text inside parens
+        col_text.gsub!(/\W/u, ' ')                     # Remove all other non-word characters      
         cols = (col_text.split(' ') - words_to_omit)
         (cols.size > 5 ? cols[-5..-1] : cols).join("_")
       end
