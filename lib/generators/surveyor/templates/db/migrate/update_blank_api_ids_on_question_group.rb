8c956b3b50b74fbba9951f9ca24fb0b99a5ef9ca
John Dzak <john.dzak@gmail.com>
2012-04-13 11:38:27 -0500

    Add survey, question, answer, response, and response set to migration that adds uuids for rows missing that.

--- lib/generators/surveyor/templates/db/migrate/update_blank_api_ids_on_question_group.rb
+++ lib/generators/surveyor/templates/db/migrate/update_blank_api_ids_on_question_group.rb
@@ -1,12 +1,18 @@
-class QG < ActiveRecord::Base
-  set_table_name 'question_groups' 
-end
+class Survey < ActiveRecord::Base; end
+class Question < ActiveRecord::Base; end
+class QuestionGroup < ActiveRecord::Base; end
+class Answer < ActiveRecord::Base; end
+class Response < ActiveRecord::Base; end
+class ResponseSet < ActiveRecord::Base; end
 
 class UpdateBlankApiIdsOnQuestionGroup < ActiveRecord::Migration
   def self.up
-    QG.where('api_id IS ?', nil).each do |qg|
-      qg.api_id = Surveyor::Common.generate_api_id
-      qg.save!
+    check = [Survey, Question, QuestionGroup, Answer, Response, ResponseSet]
+    check.each do |clazz|
+      clazz.where('api_id IS ?', nil).each do |c|
+        c.api_id = Surveyor::Common.generate_api_id
+        c.save!
+      end
     end
   end
 
