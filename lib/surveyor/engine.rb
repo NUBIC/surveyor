a6c4ba30718836b5194375376aa7795dac58e5c5
Mark Yoon <yoon@northwestern.edu>
2012-05-09 13:56:06 -0500

    cleaning up rails engine asset pipeline initializer that is no longer needed

--- lib/surveyor/engine.rb
+++ lib/surveyor/engine.rb
@@ -1,23 +1,10 @@
 require 'rails'
 require 'surveyor'
-require 'surveyor/helpers/asset_pipeline'
 require 'haml' # required for view resolution
 
 module Surveyor
   class Engine < Rails::Engine
     root = File.expand_path('../../', __FILE__)
-
     config.autoload_paths << root
-
-    initializer 'surveyor.asset_pipeline', :group => :all do |app|
-      extend Surveyor::Helpers::AssetPipeline
-
-      if asset_pipeline_enabled?
-        app.config.assets.paths += Dir[
-          "#{root}/generators/surveyor/templates/public/*",
-          "#{root}/../app/assets/*"
-        ]
-      end
-    end
   end
 end
