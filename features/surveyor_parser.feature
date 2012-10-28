NUBIC/surveyor

commit 5baa7ac32156cf18a8cce679915ab041c1051959

missed a merge commmit during a rebase. this merge closes #341

lib/surveyor/parser.rb
@@ -45,6 +45,9 @@ module Surveyor
       if block_models.include?(type)
         self.instance_eval(&block)
         if type == 'survey'
+          resolve_dependency_condition_references
+          resolve_question_correct_answers
+          report_lost_and_duplicate_references
           Surveyor::Parser.rake_trace "\n"
           Surveyor::Parser.rake_trace context[:survey].save ? "saved. " : " not saved! #{context[type.to_sym].errors.each_full{|x| x }.join(", ")} "
         else
@@ -73,6 +76,30 @@ module Surveyor
     def block_models
       %w(survey survey_section question_group)
     end
+    def report_lost_and_duplicate_references
+      raise Surveyor::ParserError, "Bad references: #{self.context[:bad_references].join("; ")}" unless self.context[:bad_references].empty?
+      raise Surveyor::ParserError, "Duplicate references: #{self.context[:duplicate_references].join("; ")}" unless self.context[:duplicate_references].empty?
+    end
+    def resolve_question_correct_answers
+      self.context[:questions_with_correct_answers].each do |question_reference_idenitifer, correct_answer_reference|
+        # Looking up references for quiz answers
+        if self.context[:answer_references][question_reference_idenitifer] &&
+             (a = self.context[:answer_references][question_reference_idenitifer][correct_answer_reference]) &&
+             a.save
+          self.context[:question_references][question_reference_idenitifer].correct_answer_id = a.id
+        else
+          self.context[:bad_references].push "q_#{question_reference_idenitifer}.correct => a_#{correct_answer_reference}"
+        end
+      end
+    end
+    def resolve_dependency_condition_references
+      self.context[:dependency_conditions].each do |dc|
+        # Looking up references to questions and answers for linking the dependency objects
+        self.context[:bad_references].push "q_#{dc.question_reference}" unless (dc.question = self.context[:question_references][dc.question_reference])
+        self.context[:answer_references][dc.question_reference] ||= {}
+        self.context[:bad_references].push "q_#{dc.question_reference}, a_#{dc.answer_reference}" if !dc.answer_reference.blank? and (dc.answer = self.context[:answer_references][dc.question_reference][dc.answer_reference]).nil?
+      end
+    end
   end
 end
 
@@ -80,11 +107,6 @@ end
 
 # Survey model
 module SurveyorParserSurveyMethods
-  # def SurveyorParserSurveyMethods.extended(obj)
-  #   attr_accessor :context_reference
-  #   attr_accessible :context_reference
-  #   after_save :report_lost_and_duplicate_references
-  # end
   def parse_and_build(context, args, original_method, reference_identifier)
     # clear context
     clear(context)
@@ -93,20 +115,18 @@ module SurveyorParserSurveyMethods
     title = args[0]
     self.attributes = ({
       :title => title,
-      :context_reference => context,
       :reference_identifier => reference_identifier }.merge(args[1] || {}))
     context[:survey] = self
   end
   def clear(context)
-    context.delete_if{|k,v| true }
-    context[:question_references] = {}
-    context[:answer_references] = {}
-    context[:bad_references] = []
-    context[:duplicate_references] = []
-  end
-  def report_lost_and_duplicate_references
-    raise Surveyor::ParserError, "Bad references: #{context_reference[:bad_references].join("; ")}" unless context_reference[:bad_references].empty?
-    raise Surveyor::ParserError, "Duplicate references: #{context_reference[:duplicate_references].join("; ")}" unless context_reference[:duplicate_references].empty?
+    context.delete_if{|k,v| true}
+    context.merge!({
+      :question_references => {},
+      :answer_references => {},
+      :bad_references => [],
+      :duplicate_references => [],
+      :dependency_conditions => [],
+      :questions_with_correct_answers => {} })
   end
 end
 
@@ -124,7 +144,14 @@ module SurveyorParserSurveySectionMethods
     context[:survey].sections << context[:survey_section] = self
   end
   def clear(context)
-    context.delete_if{|k,v| !%w(survey question_references answer_references bad_references duplicate_references).map(&:to_sym).include?(k)}
+    [ :survey_section,
+      :question_group,
+      :question,
+      :dependency,
+      :dependency_condition,
+      :answer,
+      :validation,
+      :validation_condition ].each{|k| context.delete k}
   end
 end
 
@@ -141,54 +168,55 @@ module SurveyorParserQuestionGroupMethods
     context[:question_group] = self
   end
   def clear(context)
-    context.delete_if{|k,v| !%w(survey survey_section question_references answer_references bad_references duplicate_references).map(&:to_sym).include?(k)}
+    [ :question_group,
+      :question,
+      :dependency,
+      :dependency_condition,
+      :answer,
+      :validation,
+      :validation_condition ].each{|k| context.delete k}
   end
 end
 
 # Question model
 module SurveyorParserQuestionMethods
-  # def SurveyorParserQuestionMethods.extended(obj)
-  #   attr_accessor :correct, :context_reference
-  #   attr_accessible :correct, :context_reference
-  #   before_save :resolve_correct_answers
-  # end
   def parse_and_build(context, args, original_method, reference_identifier)
     # clear context
-    context.delete_if{|k,v| %w(question dependency dependency_condition answer validation validation_condition).map(&:to_sym).include? k}
+    [ :question,
+      :dependency,
+      :dependency_condition,
+      :answer,
+      :validation,
+      :validation_condition ].each{|k| context.delete k}
 
     # build and set context
     text = args[0] || "Question"
+    hash_args = args[1] || {}
+    correct = hash_args.delete :correct
     self.attributes = ({
-      :context_reference => context,
       :question_group => context[:question_group],
       :reference_identifier => reference_identifier,
       :text => text,
       :display_type => (original_method =~ /label|image/ ? original_method : "default"),
-      :display_order => context[:survey_section].questions.size }.merge(args[1] || {}))
+      :display_order => context[:survey_section].questions.size }.merge(hash_args))
     context[:survey_section].questions << context[:question] = self
 
+    # keep reference for correct answers
+    context[:questions_with_correct_answers][self.reference_identifier] = correct unless self.reference_identifier.blank? or correct.blank?
+
     # keep reference for dependencies
-    unless reference_identifier.blank?
-      context[:duplicate_references].push "q_#{reference_identifier}" if context[:question_references].has_key?(reference_identifier)
-      context[:question_references][reference_identifier] = context[:question]
+    unless self.reference_identifier.blank?
+      context[:duplicate_references].push "q_#{self.reference_identifier}" if context[:question_references].has_key?(self.reference_identifier)
+      context[:question_references][self.reference_identifier] = context[:question]
     end
 
     # add grid answers
     if context[:question_group] && context[:question_group].display_type == "grid"
       (context[:grid_answers] || []).each do |grid_answer|
         a = context[:question].answers.build(grid_answer.attributes.reject{|k,v| %w(id api_id created_at updated_at).include?(k)})
-        context[:answer_references][reference_identifier] ||= {} unless reference_identifier.blank?
-        context[:answer_references][reference_identifier][grid_answer.reference_identifier] = a unless reference_identifier.blank? or grid_answer.reference_identifier.blank?
+        context[:answer_references][self.reference_identifier] ||= {} unless self.reference_identifier.blank?
+        context[:answer_references][self.reference_identifier][grid_answer.reference_identifier] = a unless self.reference_identifier.blank? or grid_answer.reference_identifier.blank?
       end
-    end
-  end
-
-  def resolve_correct_answers
-    unless self.correct.blank? or self.reference_identifier.blank? or self.context_reference.blank?
-      # Looking up references for quiz answers
-      self.context_reference[:answer_references][self.reference_identifier] ||= {}
-      self.context_reference[:answer_references][self.reference_identifier][correct].save
-      Surveyor::Parser.rake_trace( (self.correct_answer_id = self.context_reference[:answer_references][self.reference_identifier][self.correct].id) ? "found correct answer:#{self.correct} " : "lost! correct answer:#{self.correct} ")
     end
   end
 end
@@ -197,7 +225,8 @@ end
 module SurveyorParserDependencyMethods
   def parse_and_build(context, args, original_method, reference_identifier)
     # clear context
-    context.delete_if{|k,v| %w(dependency dependency_condition).map(&:to_sym).include? k}
+    [ :dependency,
+      :dependency_condition ].each{|k| context.delete k}
 
     # build and set context
     self.attributes = (args[0] || {})
@@ -211,45 +240,23 @@ end
 
 # DependencyCondition model
 module SurveyorParserDependencyConditionMethods
-  # def SurveyorParserDependencyConditionMethods.extended(obj)
-  #   attr_accessor :question_reference, :answer_reference, :context_reference
-  #   attr_accessible :question_reference, :answer_reference, :context_reference
-  #   before_save :resolve_references
-  # end
+  DependencyCondition.instance_eval do
+    attr_accessor :question_reference, :answer_reference
+    attr_accessible :question_reference, :answer_reference
+  end
   def parse_and_build(context, args, original_method, reference_identifier)
     # clear context
-    context.delete_if{|k,v| k == :dependency_condition}
+    context.delete :dependency_condition
 
     # build and set context
     a0, a1, a2 = args
     self.attributes = ({
-      :context_reference => context,
       :operator => a1 || "==",
       :question_reference => a0.to_s.gsub(/^q_/, ""),
       :rule_key => reference_identifier
     }.merge( a2.is_a?(Hash) ? a2 : { :answer_reference => a2.to_s.gsub(/^a_/, "") }))
     context[:dependency].dependency_conditions << context[:dependency_condition] = self
-  end
-
-  def resolve_references
-    if context_reference
-      # Looking up references to questions and answers for linking the dependency objects
-      if (self.question = context_reference[:question_references][question_reference])
-        Surveyor::Parser.rake_trace("found q_#{question_reference} ")
-      else
-        Surveyor::Parser.rake_trace("lost q_#{question_reference}! ")
-        context_reference[:bad_references].push "q_#{question_reference}"
-      end
-      unless answer_reference.blank?
-        context_reference[:answer_references][question_reference] ||= {}
-        if (self.answer = context_reference[:answer_references][question_reference][answer_reference])
-          Surveyor::Parser.rake_trace( "found a_#{answer_reference} ")
-        else
-          Surveyor::Parser.rake_trace( "lost a_#{answer_reference}!")
-          context_reference[:bad_references].push "q_#{question_reference}, a_#{answer_reference}"
-        end
-      end
-    end
+    context[:dependency_conditions] << self
   end
 end
 
@@ -257,7 +264,9 @@ end
 module SurveyorParserAnswerMethods
   def parse_and_build(context, args, original_method, reference_identifier)
     # clear context
-    context.delete_if{|k,v| %w(answer validation validation_condition reference_identifier).map(&:to_sym).include? k}
+    [ :answer,
+      :validation,
+      :validation_condition ].each{|k| context.delete k}
     attrs = { :reference_identifier => reference_identifier }.merge(parse_args(args))
 
     # add answers to grid
@@ -312,6 +321,9 @@ end
 module SurveyorParserValidationMethods
   def parse_and_build(context, args, original_method, reference_identifier)
     # clear context
+    [ :validation,
+      :validation_condition ].each{|k| context.delete k}
+
     context.delete_if{|k,v| %w(validation validation_condition).map(&:to_sym).include? k}
 
     # build and set context
@@ -324,7 +336,7 @@ end
 module SurveyorParserValidationConditionMethods
   def parse_and_build(context, args, original_method, reference_identifier)
     # clear context
-    context.delete_if{|k,v| k == :validation_condition}
+    context.delete :validation_condition
 
     # build and set context
     a0, a1 = args

features/z_redcap_parser.feature
@@ -1,6 +1,5 @@
 # The redcap parser feature should run last. If it runs in between other features that use the surveyor parser,
 # it causes the DependencyCondition before_save :resolve_references hook to stop running, causing hard to trace failures.
-@focus
 Feature: Survey import from REDCap
   As a developer
   I want to write out the survey in the DSL

commit 9ccb6030a4999a924596056eabfaed798fb76ce3

fixing grid answers leak, introduced in 5baa7ac3. closes #375. closes #377. thanks @jsurrett

features/surveyor_parser.feature
@@ -391,20 +391,20 @@ Feature: Survey parser
     Then the parser should fail with "Duplicate references: q_watch, a_1; q_watch"
 
   Scenario: Parsing with Rails validation errors
-   Given the survey
-   """
-     survey do
-      section "Usage" do
-        q_PLACED_BAG_1 "Is the bag placed?", :pick => :one
-        a_1 "Yes"
-        a_2 "No"
-        a_3 "Refused"
+    Given the survey
+    """
+      survey do
+        section "Usage" do
+          q_PLACED_BAG_1 "Is the bag placed?", :pick => :one
+          a_1 "Yes"
+          a_2 "No"
+          a_3 "Refused"
+        end
       end
-    end
-   """
-   Then the parser should fail with "Survey not saved: Title can't be blank"
+    """
+    Then the parser should fail with "Survey not saved: Title can't be blank"
 
-   Scenario: Parsing bad shortcuts
+  Scenario: Parsing bad shortcuts
     Given the survey
     """
       survey "shortcuts" do
@@ -416,3 +416,36 @@ Feature: Survey parser
       end
     """
     Then the parser should fail with "\"quack\" is not a surveyor method."
+
+  Scenario: Clearing grid answers
+    Given I parse
+    """
+      survey "Grids" do
+        section "Leaking" do
+          grid "How would you rate the following?" do
+            a "bad"
+            a "neutral"
+            a "good"
+            q "steak" , :pick => :one
+            q "chicken", :pick => :one
+            q "fish", :pick => :one
+          end
+          grid "How do you feel about the following?" do
+            a "sad"
+            a "indifferent"
+            a "happy"
+            q "births" , :pick => :one
+            q "weddings", :pick => :one
+            q "funerals", :pick => :one
+          end
+        end
+      end
+    """
+    Then there should be 18 answers with:
+      | text        | display_order |
+      | bad         | 0             |
+      | neutral     | 1             |
+      | good        | 2             |
+      | sad         | 0             |
+      | indifferent | 1             |
+      | happy       | 2             |

lib/surveyor/parser.rb
@@ -165,7 +165,8 @@ module SurveyorParserSurveyMethods
       :bad_references => [],
       :duplicate_references => [],
       :dependency_conditions => [],
-      :questions_with_correct_answers => {} })
+      :questions_with_correct_answers => {}
+    })
   end
 end
 
@@ -185,6 +186,7 @@ module SurveyorParserSurveySectionMethods
   def clear(context)
     [ :survey_section,
       :question_group,
+      :grid_answers,
       :question,
       :dependency,
       :dependency_condition,
@@ -208,12 +210,14 @@ module SurveyorParserQuestionGroupMethods
   end
   def clear(context)
     [ :question_group,
+      :grid_answers,
       :question,
       :dependency,
       :dependency_condition,
       :answer,
       :validation,
       :validation_condition ].each{|k| context.delete k}
+    context[:grid_answers] = []
   end
 end
 
@@ -310,8 +314,7 @@ module SurveyorParserAnswerMethods
 
     # add answers to grid
     if context[:question_group] && context[:question_group].display_type == "grid"
-      context[:grid_answers] ||= []
-      self.attributes = ({:display_order => [:grid_answers].size}.merge(attrs))
+      self.attributes = ({:display_order => context[:grid_answers].size}.merge(attrs))
       context[:grid_answers] << context[:answer] = self
     else
       self.attributes = ({:display_order => context[:question].answers.size}.merge(attrs))