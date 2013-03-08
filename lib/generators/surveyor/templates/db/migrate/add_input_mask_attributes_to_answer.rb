be58f10ef19d7b6c0c66969a604cdb83b075f9c4
Mark Yoon <yoon@northwestern.edu>
2013-03-08 10:05:52 -0600

    rails 3.0 doesn't use 'change' migration syntax

--- lib/generators/surveyor/templates/db/migrate/add_input_mask_attributes_to_answer.rb
+++ lib/generators/surveyor/templates/db/migrate/add_input_mask_attributes_to_answer.rb
@@ -1,7 +1,12 @@
 # encoding: UTF-8
 class AddInputMaskAttributesToAnswer < ActiveRecord::Migration
-  def change
-  	add_column :answers, :input_mask, :string
-  	add_column :answers, :input_mask_placeholder, :string
+  def self.up
+    add_column :answers, :input_mask, :string
+    add_column :answers, :input_mask_placeholder, :string
+  end
+
+  def self.down
+    remove_column :answers, :input_mask
+    remove_column :answers, :input_mask_placeholder
   end
 end
