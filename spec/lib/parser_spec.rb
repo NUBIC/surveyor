5c4df151e23bf9c5cc1ad0500c645da853d70afe
Mark Yoon <yoon@northwestern.edu>
2013-03-29 11:12:09 -0500

    refactoring for readability

--- spec/lib/parser_spec.rb
+++ spec/lib/parser_spec.rb
@@ -2,33 +2,31 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Surveyor::Parser do
-  before(:each) do
-    @parser = Surveyor::Parser.new
-  end
+  let(:parser){ Surveyor::Parser.new }
   it "should translate shortcuts into full model names" do
-    @parser.send(:full, "section").should == "survey_section"
-    @parser.send(:full, "g").should == "question_group"
-    @parser.send(:full, "repeater").should == "question_group"
-    @parser.send(:full, "label").should == "question"
-    @parser.send(:full, "vc").should == "validation_condition"
-    @parser.send(:full, "vcondition").should == "validation_condition"
+    parser.send(:full, "section").should == "survey_section"
+    parser.send(:full, "g").should == "question_group"
+    parser.send(:full, "repeater").should == "question_group"
+    parser.send(:full, "label").should == "question"
+    parser.send(:full, "vc").should == "validation_condition"
+    parser.send(:full, "vcondition").should == "validation_condition"
   end
   it "should translate 'condition' based on context" do
-    @parser.send(:full, "condition").should == "dependency_condition"
-    @parser.send(:full, "c").should == "dependency_condition"
-    @parser.context[:validation] = Validation.new
-    @parser.send(:full, "condition").should == "validation_condition"
-    @parser.send(:full, "c").should == "validation_condition"
-    @parser.context[:validation] = nil
-    @parser.send(:full, "condition").should == "dependency_condition"
-    @parser.send(:full, "c").should == "dependency_condition"
+    parser.send(:full, "condition").should == "dependency_condition"
+    parser.send(:full, "c").should == "dependency_condition"
+    parser.context[:validation] = Validation.new
+    parser.send(:full, "condition").should == "validation_condition"
+    parser.send(:full, "c").should == "validation_condition"
+    parser.context[:validation] = nil
+    parser.send(:full, "condition").should == "dependency_condition"
+    parser.send(:full, "c").should == "dependency_condition"
   end
   it "should not translate bad shortcuts" do
-    @parser.send(:full, "quack").should == "quack"
-    @parser.send(:full, "grizzly").should == "grizzly"
+    parser.send(:full, "quack").should == "quack"
+    parser.send(:full, "grizzly").should == "grizzly"
   end
   it "should identify models that take blocks" do
-    @parser.send(:block_models).should == %w(survey survey_section question_group)
+    parser.send(:block_models).should == %w(survey survey_section question_group)
   end
   it "should return a survey object" do
     Surveyor::Parser.new.parse("survey 'hi' do\n end").is_a?(Survey).should be_true
@@ -37,7 +35,7 @@ describe Surveyor::Parser do
 
   context 'when a translation is specified as a Hash' do
     it 'should should treat the hash as an inline translation' do
-      survey = <<END
+      survey_text = <<END
 survey "One language is never enough" do
       translations :es => {"title"=>"Un idioma nunca es suficiente", "survey_sections"=>{"one"=>{"title"=>"Uno"}}, "question_groups"=>{"hello"=>{"text"=>"¡Hola!"}}, "questions"=>{"name"=>{"text"=>"¿Cómo se llama Usted?", "answers"=>{"name"=>{"help_text"=>"Mi nombre es..."}}}}}
       section_one "One" do
@@ -48,10 +46,10 @@ survey "One language is never enough" do
       end
     end
 END
-      s = Surveyor::Parser.new.parse(survey)
-      s.is_a?(Survey).should == true
-      s.translations.size.should == 1
-      s.translation(:es)['title'].should == "Un idioma nunca es suficiente"
+      survey = Surveyor::Parser.new.parse(survey_text)
+      survey.is_a?(Survey).should == true
+      survey.translations.size.should == 1
+      survey.translation(:es)['title'].should == "Un idioma nunca es suficiente"
     end
   end
 
@@ -61,11 +59,13 @@ END
       it 'should look for the translation file relative to pwd' do
         Dir.mktmpdir do |dir|
           FileUtils.cd(dir) do
-            t = YAML::dump({'title' => 'Un idioma nunca es suficiente'})
-            tf = Tempfile.new('surveyor:parser_spec.rb',dir);tf.write(t);tf.flush
-                  s = <<END
+            translation_yaml = YAML::dump({'title' => 'Un idioma nunca es suficiente'})
+            translation_temp_file = Tempfile.new('parser_spec_translation.yml',dir)
+            translation_temp_file.write(translation_yaml)
+            translation_temp_file.flush
+            survey_text = <<END
 survey "One language is never enough" do
-      translations :es =>'#{File.basename(tf.path)}'
+      translations :es =>'#{File.basename(translation_temp_file.path)}'
       section_one "One" do
         g_hello "Hello" do
           q_name "What is your name?"
@@ -74,8 +74,10 @@ survey "One language is never enough" do
       end
     end
 END
-            sf = Tempfile.new('surveyor:parser_spec.rb',dir);sf.write(s);sf.flush
-            Surveyor::Parser.parse(File.read(sf.path))
+            survey_temp_file = Tempfile.new('parser_spec_survey.rb',dir)
+            survey_temp_file.write(survey_text)
+            survey_temp_file.flush
+            Surveyor::Parser.parse(File.read(survey_temp_file.path))
             survey = Survey.where(:title=>'One language is never enough').first
             survey.nil?.should == false
             survey.translation(:es)['title'].should == "Un idioma nunca es suficiente"
@@ -86,11 +88,13 @@ END
     context 'when the survey filename is given' do
       it 'should look for the translation file relative to the survey directory' do
         Dir.mktmpdir do |dir|
-          t = YAML::dump({'title' => 'Un idioma nunca es suficiente'})
-            tf = Tempfile.new('surveyor:parser_spec.rb',dir);tf.write(t);tf.flush
-                  s = <<END
+          translation_yaml = YAML::dump({'title' => 'Un idioma nunca es suficiente'})
+            translation_temp_file = Tempfile.new('surveyor:parser_spec.rb',dir)
+            translation_temp_file.write(translation_yaml)
+            translation_temp_file.flush
+            survey_text = <<END
 survey "One language is never enough" do
-      translations :es =>'#{File.basename(tf.path)}'
+      translations :es =>'#{File.basename(translation_temp_file.path)}'
       section_one "One" do
         g_hello "Hello" do
           q_name "What is your name?"
@@ -99,9 +103,10 @@ survey "One language is never enough" do
       end
     end
 END
-
-          sf = Tempfile.new('surveyor:parser_spec.rb',dir);sf.write(s);sf.flush
-          Surveyor::Parser.parse_file(sf.path)
+          survey_temp_file = Tempfile.new('surveyor:parser_spec.rb',dir)
+          survey_temp_file.write(survey_text)
+          survey_temp_file.flush
+          Surveyor::Parser.parse_file(survey_temp_file.path)
           survey = Survey.where(:title=>'One language is never enough').first
           survey.nil?.should == false
           survey.translation(:es)['title'].should == "Un idioma nunca es suficiente"
