1656342cee451fd0912ef03abb16930e74d3cfb6
Mark Yoon <yoon@northwestern.edu>
2014-03-24 15:11:13 -0500

    updating stacktests

--- stacktests.sh
+++ stacktests.sh
@@ -4,13 +4,12 @@ echo ""
 
 # Set the prompt for the select command
 PS3="Choose a stack or 'q' to quit: "
-export CUCUMBER_OPTS="--format CucumberSpinner::ProgressBarFormatter"
 export SPEC_OPTS="--format Fuubar --color spec"
 export BUNDLER_VERSION=1.5.3
 
 options=""
 rubies=(2.0.0 2.1.1)
-rails_versions=(rails_3.2 rails_4.0)
+rails_versions=(3.2.17 4.0.0)
 
 for i in "${rubies[@]}"
 do
@@ -33,13 +32,13 @@ select combo in $options; do
         export RAILS_VERSION=$j
         source ~/.rvm/scripts/rvm
         rvm use $CI_RUBY@surveyor-$RAILS_VERSION --create
-        read -p "Press [Enter] to run tests on $CI_RUBY@$RAILS_VERSION..."
+        read -p "Press [Enter] to run tests on $CI_RUBY@surveyor-$RAILS_VERSION..."
         gem list -i bundler -v $BUNDLER_VERSION
         if [ $? -ne 0 ]; then
           gem install bundler -v $BUNDLER_VERSION
         fi
         bundle _${BUNDLER_VERSION}_ update
-        bundle _${BUNDLER_VERSION}_ exec rake testbed spec cucumber cucumber:wip
+        bundle _${BUNDLER_VERSION}_ exec rake testbed spec
       done
     done
     ;;
@@ -52,7 +51,7 @@ select combo in $options; do
     export RAILS_VERSION=${arr[1]}
     source ~/.rvm/scripts/rvm
     rvm use $CI_RUBY@surveyor-$RAILS_VERSION --create
-    read -p "Press [Enter] to run tests on $CI_RUBY@$RAILS_VERSION..."
+    read -p "Press [Enter] to run tests on $CI_RUBY@surveyor-$RAILS_VERSION..."
     gem list -i bundler -v $BUNDLER_VERSION
     if [ $? -ne 0 ]; then
       gem install bundler -v $BUNDLER_VERSION
