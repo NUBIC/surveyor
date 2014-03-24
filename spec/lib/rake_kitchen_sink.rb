115ed1e3a374e9b1638746a2e1c30521707685a3
Mark Yoon <yoon@northwestern.edu>
2014-03-24 12:21:14 -0500

    moving parser cucumber feature to parser spec. moving fixture surveys to fixture directory

--- spec/lib/rake_kitchen_sink.rb
+++ spec/lib/rake_kitchen_sink.rb
@@ -11,21 +11,21 @@ describe Surveyor::Parser do
   it "should return properly parse the kitchen sink survey" do
     ENV["FILE"]="surveys/kitchen_sink_survey.rb"
     @rake["surveyor"].invoke
-    
+
     Survey.count.should == 1
-    SurveySection.count.should == 2 
-    Question.count.should == 44 
-    Answer.count.should == 241 
-    Dependency.count.should == 6 
-    DependencyCondition.count.should == 9 
+    SurveySection.count.should == 2
+    Question.count.should == 44
+    Answer.count.should == 241
+    Dependency.count.should == 6
+    DependencyCondition.count.should == 9
     QuestionGroup.count.should == 6
 
     Survey.all.map(&:destroy)
   end
   it "should return properly parse a UTF8 survey" do
-    ENV["FILE"]="../spec/lib/chinese_survey.rb"
+    ENV["FILE"]="../spec/fixtures/chinese_survey.rb"
     @rake["surveyor"].invoke
-    
+
     Survey.count.should == 1
     SurveySection.count.should == 1
     Question.count.should == 3
