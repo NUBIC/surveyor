fc8fc83567fed95a63ec5e1158d4341d63067206
Bojan Percevic <percevic@me.com>
2016-05-02 15:36:42 -0400

    updated survey submission

--- lib/surveyor/surveyor_json_methods.rb
+++ lib/surveyor/surveyor_json_methods.rb
@@ -6,14 +6,18 @@ module Surveyor
     extend ActiveSupport::Concern
 
     def survey
-      # see questions.json.rabl
+      render json: {
+        message: 'Missing "employee_id" key. We need to be able to associate the employee with the survey taken.'
+      }, status: :bad_request unless params[:employee_id].present?
+
       all_surveys = Survey.where(:access_code => params[:survey_access_code]).order("survey_version DESC")
       survey = if params[:survey_version].blank?
         all_surveys.first
       else
         all_surveys.where(:survey_version => params[:survey_version]).first
       end
-      params[:employee_id] ||= nil
+
+      # must be @var because of rabl (see questions.json.rabl)
       @response_set = ResponseSet.create(:survey => survey, :user_id => params[:employee_id])
       respond_to do |format|
         format.json
@@ -22,52 +26,32 @@ module Surveyor
 
     def submit
       render json: {
-        message: 'Missing "r" key.'
+        message: 'Missing "r" key (Required to generate survey responses.)'
       }, status: :bad_request unless params[:r].present?
 
-      p "submit.params", params.to_json
-
-      ResponseSet.transaction do
-        response_set = ResponseSet.includes({:responses => :answer}).where(:access_code => params[:response_access_code]).first
-        if response_set
-          # response_set.update_from_ui_hash(params[:r])
-
-          params[:r].each do |key, val|
-            response_hash = val.except :api_id, :id
-            response = Response.new(response_set: response_set)
-            begin
-              response.update(response_hash)
-            rescue Exception => e
-              p "update error", e.message
-            end
-            p "response_hash", response_hash
-            p "response", response
-          end
-
-          # create new responses from params[:r]:
-          # "{
-          #   \"1\": {
-          #     \"question_id\": \"1\",
-          #     \"api_id\": \"5eceee3c-bc73-4f6b-9bf1-3bbebc4addf0\",
-          #     \"answer_id\": \"2\"
-          #   },
-          #   \"2\": {
-          #     \"question_id\": \"2\",
-          #     \"api_id\": \"6bf1dc2d-49eb-46d2-9a38-65dd533ea486\",
-          #     \"answer_id\": \"6\"
-          #   }
-          # }"
-
-          response_set.complete!
-          render json: {
-            message: 'Survey submitted!'
-          }, status: :ok
-        else
-          render json: {
-            message: 'ResponseSet not found.'
-          }, status: :not_found
-          false
+      response_set = ResponseSet.includes({:responses => :answer}).where(:access_code => params[:response_access_code]).first
+      if response_set
+        # iterate through each question/answer and create a Response
+        params[:r].each do |key, val|
+          response_hash = val.except :api_id, :id
+          response = Response.new(response_set: response_set)
+          response.update(response_hash)
+          response.save
         end
+
+        # this just sets the completed_at field
+        response_set.complete!
+
+        render json: {
+          message: 'Survey successfully submitted.',
+          data: {
+            survey: response_set.survey
+          }
+        }, status: :ok
+      else
+        render json: {
+          message: 'ResponseSet not found.'
+        }, status: :not_found
       end
     end
 
