1dbb627212964a3175691a32fa3074680249e5ac
Todd Parsnick <todd.parsnick@ucsf.edu>
2016-10-07 12:34:03 -0700

    Fixed class name

--- lib/surveyor/models/survey_methods_multitenant.rb
+++ lib/surveyor/models/survey_methods_multitenant.rb
@@ -3,7 +3,7 @@ require 'rabl'
 
 module Surveyor
   module Models
-    module SurveyMethods
+    module SurveyMethodsMultitenant
       extend ActiveSupport::Concern
       include ActiveModel::Validations
       include ActiveModel::ForbiddenAttributesProtection
@@ -17,7 +17,7 @@ module Surveyor
 
         # Validations
         validates_presence_of :title
-        #validates_uniqueness_of :survey_version, :scope => :access_code, :message => "survey with matching access code and version already exists"   #this validation (including study_id scope) will be added back in the multitenant project implementation of this module.
+        #validates_uniqueness_of :survey_version, :scope => :access_code, :message => "survey with matching access code and version already exists"   #this validation (including study_id scope) will be added back in the multitenant project (using acts_as_tenant) implementation of this module.
 
         # Derived attributes
         before_save :generate_access_code
